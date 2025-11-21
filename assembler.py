9b79c408cf06299dbd0c08609efd101472ee1584
Masha Lifshin <mlifshin@mozilla.com>
2025-11-21 14:14:24 -0800

    Fixed command type bug, added C instruction parsing. Next step is creating the comp bits.

--- assembler.py
+++ assembler.py
@@ -1,17 +1,31 @@
 INPUT_FILENAME = "add.asm"
 OUTPUT_FILENAME = "add.hack"
 
+A_INSTRUCTION = "A_INSTRUCTION"
+C_INSTRUCTION = "C_INSTRUCTION"
+L_INSTRUCTION = "L_INSTRUCTION"
+
 def command_type(command):
     if "@" in command[0]:
-        return 'A_COMMAND'
+        return A_INSTRUCTION
     elif '=' in command:
-        return 'C_COMMAND'
+        return C_INSTRUCTION
     elif '(' in command and ')' in command:
-        return 'L_COMMAND'
+        return L_INSTRUCTION
     else:
         raise ValueError("WTF >:( what command is this")
-    
-def jump(command):
+
+# jump    j j j   effect:
+#
+# null    0 0 0   no jump
+# JGT     0 0 1   if comp > 0 jump
+# JEQ     0 1 0   if comp = 0 jump
+# JGE     0 1 1   if comp ≥ 0 jump
+# JLT     1 0 0   if comp < 0 jump
+# JNE     1 0 1   if comp ≠ 0 jump
+# JLE     1 1 0   if comp ≤ 0 jump
+# JMP     1 1 1   Unconditional jump
+def get_jump_bits(command):
     if 'JGT' in command:
         return "001"
     elif "JEQ" in command:
@@ -28,8 +42,18 @@ def jump(command):
         return "111"
     else:
         return "000"
-    
-def dest(command):
+
+# dest    d d d   effect: the value is stored in:
+#
+# null    0 0 0   the value is not stored
+# M       0 0 1   RAM[A]
+# D       0 1 0   D   register
+# DM      0 1 1   D register and RAM[A]
+# A       1 0 0   A register
+# AM      1 0 1   A register and RAM[A]
+# AD      1 1 0   A register and D register
+# ADM     1 1 1   A register, D register, and RAM[A]
+def get_dest_bits(command):
     destination_substring = command.split("=")[0]
     if destination_substring == "M":
         return "001"
@@ -50,7 +74,35 @@ def dest(command):
     else:
         raise ValueError(f"Invalid code {destination_substring}")
 
+
+#     comp        c c c c c c
+#
+# a=0     a=1
+#
+# 0               1 0 1 0 1 0
+# 1               1 1 1 1 1 1
+# -1              1 1 1 0 1 0
+# D               0 0 1 1 0 0
+# A       M       1 1 0 0 0 0
+# !D              0 0 1 1 0 1
+# !A      !M      1 1 0 0 0 1
+# -D              0 0 1 1 1 1
+# -A      -M      1 1 0 0 1 1
+# D+1             0 1 1 1 1 1
+# A+1     M+1     1 1 0 1 1 1
+# D-1             0 0 1 1 1 0
+# A-1     M-1     1 1 0 0 1 0
+# D+A     D+M     0 0 0 0 1 0
+# D-A     D-M     0 1 0 0 1 1
+# A-D     M-D     0 0 0 1 1 1
+# D&A     D&M     0 0 0 0 0 0
+# D|A     D|M     0 1 0 1 0 1
+def get_comp_bits(command):
+    pass
+
+
 def convert_to_binary(value):
+    print(f"convert_to_binary input: {value}")
     value_int = int(value)
     if value_int >= 0:
         bin_str = '0{0:015b}'.format(value_int)
@@ -60,29 +112,64 @@ def convert_to_binary(value):
         bin_str = "0"+str(bin_value)[2:]
     return bin_str
 
+# TODO: Handle invalid instructions
+def parse_c_instruction(command):
+    # D = D+1 ; JLE
+    # JGE
+    # D = D+1
+    # A ; JLE
+    comp, dest, jump = None
+    if (command.contains("=")):
+        # there will be a dest and comp
+        # there may or may not be a jump
+        dest, the_rest = command.split("=")
+        if (the_rest.contains(";")):
+            comp, jump = the_rest.split(";")
+        else:
+            comp = the_rest
+            jump = None
+    else:
+        # there will not be any dest or comp
+        # there will be a jump
+        dest = None
+        if (command.contains(";")):
+            comp, jump = command.split(";")
+    jump_bits = get_jump_bits(jump)
+    dest_bits = get_dest_bits(dest)
+    comp_bits = get_comp_bits(comp)
+
+    return comp_bits, dest_bits, jump_bits
+
 def parser(command):
     # TODO make sure it handles whitespace properly
     # TODO semicolons
-    clean_command = command.strip()
-    command_type_value = command_type(clean_command)
-    if command_type_value == 'A_INSTRUCTION':
-        return '0{0:15b}'.format(6)
-    elif command_type_value == "C_INSTRUCTION":
-        jump_bits = jump(command)
-        dest_bits = dest(command)
-    return bytecode
+    print(f"command: {command}")
 
+    clean_command = command.strip()
+    print(f"clean_command: {clean_command}")
+
+    command_type_value = command_type(clean_command)
+    print(f"command_type_value: {command_type_value}")
+    if command_type_value == A_INSTRUCTION:
+        cleaner_command = clean_command[1:]
+        return convert_to_binary(cleaner_command)
+    elif command_type_value == C_INSTRUCTION:
+        comp, dest, jump = parse_c_instruction(clean_command)
+        return f"1{comp}{dest}{jump}"
 
 
 if __name__ == "__main__":
     output = []
     for line in open(INPUT_FILENAME).readlines():
+        print("\n")
         if not line.strip():
             continue
-        output.append(parser(line.replace("\n","")))
-    
+        parsed = parser(line.replace("\n",""))
+        print(f"parsed: {parsed}")
+        if not parsed:
+            continue
+        output.append(parsed)
+
     output_with_newlines = [x+"\n" for x in output]
     with open(OUTPUT_FILENAME, 'w') as outfile:
         outfile.writelines(output_with_newlines)
-
-  
